3d844a6d27e29a05a4252ed5d9c5ddacf16e8b48
Alexey Venderov <alexey@cobalt.io>
2019-09-07 09:33:41 +0200

    first commit

---
+++ src/main/java/com/revolut/transfer/Launcher.java
@@ -0,0 +1,9 @@
+package com.revolut.transfer;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
---
+++ src/test/java/com/revolut/transfer/SimpleTest.java
@@ -0,0 +1,13 @@
+package com.revolut.transfer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SimpleTest {
+
+    @Test
+    void test() {
+        assertThat(true).isTrue();
+    }
+}
