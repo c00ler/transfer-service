8f4637e6c42cb57143c410555ed1f002a0b91212
Alexey Venderov <alexey@cobalt.io>
2019-09-07 10:09:43 +0200

    Add javalin and logger configuration

--- src/main/java/com/revolut/transfer/Launcher.java
+++ src/main/java/com/revolut/transfer/Launcher.java
@@ -1,9 +1,15 @@
 package com.revolut.transfer;
 
+import io.javalin.Javalin;
+
 public class Launcher {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        var app = Javalin.create(config -> {
+            config.showJavalinBanner = false;
+        }).start(7000);
+
+        app.get("/", ctx -> ctx.result("Hello World"));
     }
 
 }
